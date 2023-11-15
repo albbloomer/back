fc409facad4fa38fb8917cdbc1cbffc12b9666ab
albbloomer <a_l_b_bloomer@naver.com>
2023-11-15 10:14:38 +0900

    [cote] feat : 알고리즘 틀 추가

---
+++ src/main/java/com/example/back/cote/CotePlayground1.java
@@ -0,0 +1,11 @@
+package com.example.back.cote;
+
+public class CotePlayground1 {
+
+    public static Integer problem_1_result() {
+        int leftNumber = 1;
+        int rightNumber = 2;
+
+        return leftNumber + rightNumber;
+    }
+}
