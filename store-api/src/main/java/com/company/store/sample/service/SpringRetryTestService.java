ab043f8d4736712484f482d60ff9172ff61367a9
albbloomer <a_l_b_bloomer@naver.com>
2023-12-16 16:51:14 +0900

    feat: retry test recover

--- store-api/src/main/java/com/company/store/sample/service/SpringRetryTestService.java
+++ store-api/src/main/java/com/company/store/sample/service/SpringRetryTestService.java
@@ -1,5 +1,6 @@
 package com.company.store.sample.service;
 
+import org.springframework.retry.annotation.Recover;
 import org.springframework.stereotype.Service;
 
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ public class SpringRetryTestService {
     private int counter = 0;
     private static final Logger logger = LoggerFactory.getLogger(SpringRetryTestService.class);
 
+    // case 1
     @Retryable(
             maxAttempts = 3,
             backoff = @Backoff(delay = 1000),
@@ -27,5 +29,29 @@ public class SpringRetryTestService {
         }
 
         throw new RuntimeException("Failed to get cat image.");
+    }
+
+    // case 2 -> recover option
+    @Retryable(
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000),
+            retryFor = RuntimeException.class, // include deprecated
+            recover = "recover"
+    )
+    public String catImageByRecover(final Long id) {
+        counter++;
+        logger.info("counter: [{}]", counter);
+
+        if (counter % 2 == 0) {
+            return "cat_" + id + "_image.png";
+        }
+
+        throw new RuntimeException("Failed to get cat image.");
+    }
+
+    @Recover
+    public String recover(final Exception exception, final Long id) {
+        logger.info("recover: [{}]", exception.getMessage(), exception);
+        return "recover cat";
     }
 }
