d9fd8fe8224617fae4be1be80d7c62483849720e
albbloomer <a_l_b_bloomer@naver.com>
2024-01-01 21:34:23 +0900

    feat : CoteHighScoreKit - 가장 먼 노드

--- cote/src/main/java/com/company/programers/CoteHighScoreKit1.java
+++ cote/src/main/java/com/company/programers/CoteHighScoreKit1.java
@@ -272,4 +272,47 @@ public class CoteHighScoreKit1 {
             return max;
         }
     }
+
+    static class solution_9 {
+        public int solution(int n, int[][] edge) {
+            ArrayList<Integer>[] path = new ArrayList[n];
+            ArrayList<Integer> bfs = new ArrayList<Integer>();
+            int answer = 0;
+            int[] dist = new int[n];
+            dist[0] = 1;
+            int max = 0;
+
+            for ( int i = 0; i < edge.length; i++) {
+                int num1 = edge[i][0] - 1;
+                int num2 = edge[i][1] - 1;
+                if(path[num1] == null)
+                    path[num1] = new ArrayList<Integer>();
+                if(path[num2] == null)
+                    path[num2] = new ArrayList<Integer>();
+                path[num1].add(num2);
+                path[num2].add(num1);
+            }
+
+            bfs.add(0);
+            while(!bfs.isEmpty()) {
+                int idx = bfs.get(0);
+                bfs.remove(0);
+                while(!path[idx].isEmpty()) {
+                    int num = path[idx].get(0);
+                    path[idx].remove(0);
+                    bfs.add(num);
+                    if(dist[num] == 0) {
+                        dist[num] = dist[idx] + 1;
+                        max = dist[num];
+                    }
+                }
+            }
+            for(int i = 0; i < n; i++) {
+                if(dist[i] == max)
+                    answer++;
+            }
+
+            return answer;
+        }
+    }
 }
