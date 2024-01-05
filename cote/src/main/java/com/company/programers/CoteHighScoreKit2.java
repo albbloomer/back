8a04e0623c205b64519287f2f0f8e20a3c99faf8
albbloomer <a_l_b_bloomer@naver.com>
2024-01-05 21:06:15 +0900

    feat : CoteHighScoreKit - 디스크 컨트롤러

--- cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
+++ cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
@@ -69,8 +69,48 @@ public class CoteHighScoreKit2 {
      */
     static class Solution_3 {
         public int solution(int[][] jobs) {
+            PriorityQueue<Work> startQueue = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
+            PriorityQueue<Work> timeQueue = new PriorityQueue<>((o1, o2) -> o1.time - o2.time);
+
+            for(int[] work : jobs) {
+                startQueue.offer(new Work(work[0], work[1]));
+            }
+
             int answer = 0;
-            return answer;
+            int startTime = startQueue.peek().start;
+            while(!startQueue.isEmpty()){
+                while(true){
+                    if(!startQueue.isEmpty() && startTime >= startQueue.peek().start)
+                        timeQueue.offer(startQueue.poll());
+                    else
+                        break;
+                }
+
+                if(!timeQueue.isEmpty()) {
+                    Work task = timeQueue.poll();
+                    answer += startTime + task.time - task.start;
+                    startTime += task.time;
+                }
+                else{
+                    Work task = startQueue.poll();
+                    answer += task.time;
+                    startTime = task.time + task.start;
+                }
+
+                while(!timeQueue.isEmpty())
+                    startQueue.offer(timeQueue.poll());
+            }
+
+            return answer / jobs.length;
+        }
+        public static class Work {
+            private final int start;
+            private final int time;
+
+            public Work(int start, int time){
+                this.start = start;
+                this.time = time;
+            }
         }
     }
 }
