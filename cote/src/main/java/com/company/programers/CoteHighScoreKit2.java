26011f6b62b12225323e9c2c09ab980be5398ff1
albbloomer <a_l_b_bloomer@naver.com>
2024-01-06 13:31:11 +0900

    feat : CoteHighScoreKit - 네트워크

--- cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
+++ cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
@@ -171,4 +171,31 @@ public class CoteHighScoreKit2 {
             return list.stream().mapToInt(i -> i).toArray();
         }
     }
+
+    // Todo : 네트워크 - DFS/BFS
+    static class Solution_7 {
+        public int solution(int n, int[][] computers) {
+            boolean[] visited = new boolean[n];
+            int answer = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    dfs(computers, visited, i);
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
+        private void dfs(int[][] computers, boolean[] visited, int start) {
+            visited[start] = true;
+
+            for (int i = 0; i < computers.length; i++) {
+                if (computers[start][i] == 1 && !visited[i]) {
+                    dfs(computers, visited, i);
+                }
+            }
+        }
+    }
 }
