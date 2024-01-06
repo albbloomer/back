380a5916e404ae986388a996c0afb74eb9c43130
albbloomer <a_l_b_bloomer@naver.com>
2024-01-06 11:38:05 +0900

    feat : CoteHighScoreKit - 모의고사

--- cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
+++ cote/src/main/java/com/company/programers/CoteHighScoreKit2.java
@@ -138,4 +138,37 @@ public class CoteHighScoreKit2 {
             return sb.toString();
         }
     }
+
+    // Todo : 모의고사- 완전탐색
+    static class Solution_6 {
+        public int[] solution(int[] answers) {
+            // 각 수포자의 찍기 패턴
+            int[] supoja1 = {1, 2, 3, 4, 5};
+            int[] supoja2 = {2, 1, 2, 3, 2, 4, 2, 5};
+            int[] supoja3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+            // 각 수포자가 맞힌 문제 수
+            int[] score = new int[3];
+
+            for (int i = 0; i < answers.length; i++) {
+                if (answers[i] == supoja1[i % supoja1.length]) score[0]++;
+                if (answers[i] == supoja2[i % supoja2.length]) score[1]++;
+                if (answers[i] == supoja3[i % supoja3.length]) score[2]++;
+            }
+
+            // 가장 높은 점수 찾기
+            int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+
+            // 가장 높은 점수를 받은 사람 찾기
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                if (score[i] == maxScore) {
+                    list.add(i + 1);
+                }
+            }
+
+            // 결과 반환
+            return list.stream().mapToInt(i -> i).toArray();
+        }
+    }
 }
