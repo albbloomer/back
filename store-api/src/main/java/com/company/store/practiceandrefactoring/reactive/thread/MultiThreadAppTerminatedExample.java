4d169ba9b2bf731b9c44d93339f9523db448f118
albbloomer <a_l_b_bloomer@naver.com>
2024-02-11 17:42:52 +0900

    feat : 멀티스레드

---
+++ store-api/src/main/java/com/company/store/practiceandrefactoring/reactive/thread/MultiThreadAppTerminatedExample.java
@@ -0,0 +1,41 @@
+package com.company.store.practiceandrefactoring.reactive.thread;
+
+public class MultiThreadAppTerminatedExample {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(new MyRunnable(i));
+            thread.start();
+        }
+
+        System.out.println("메인 스레드 종료");
+
+    }
+    static class MyRunnable implements Runnable{
+
+        private final int threadId;
+
+        public MyRunnable(int threadId) {
+
+            this.threadId = threadId;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": 스레드 실행 중...");
+            firstMethod(threadId);
+        }
+
+        private void firstMethod(int threadId) {
+
+            int localValue = threadId + 100;
+            secondMethod(localValue);
+
+        }
+
+        private void secondMethod(int localValue) {
+            String objectReference = threadId + ": Hello World";
+            System.out.println(Thread.currentThread().getName() + " : 스레드 ID : " + threadId + ", Value:" + localValue);
+        }
+    }
+}
