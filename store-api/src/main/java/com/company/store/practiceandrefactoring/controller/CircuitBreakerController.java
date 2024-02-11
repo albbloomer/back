c0330b872d944e7dfa0539fe320acc739228f286
albbloomer <a_l_b_bloomer@naver.com>
2024-02-12 08:28:40 +0900

    feat : circuitbreaker 실습 포맷

--- store-api/src/main/java/com/company/store/practiceandrefactoring/controller/CircuitBreakerController.java
+++ store-api/src/main/java/com/company/store/practiceandrefactoring/controller/CircuitBreakerController.java
@@ -1,4 +1,22 @@
 package com.company.store.practiceandrefactoring.controller;
 
+import com.company.store.common.ResponseDto;
+import com.company.store.practiceandrefactoring.service.CircuitBreakerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/circuit")
+@RestController
 public class CircuitBreakerController {
+
+    private final CircuitBreakerService circuitBreakerService;
+
+    @GetMapping
+    public ResponseDto<String> test() {
+
+        return ResponseDto.ok("haha");
+    }
 }
---
+++ store-api/src/main/java/com/company/store/practiceandrefactoring/service/CircuitBreakerService.java
@@ -0,0 +1,7 @@
+package com.company.store.practiceandrefactoring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CircuitBreakerService {
+}
