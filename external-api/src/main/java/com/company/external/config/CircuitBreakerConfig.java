6f9a4df7509754573b47c3913baebfb85a8868e8
albbloomer <a_l_b_bloomer@naver.com>
2024-02-12 19:30:34 +0900

    feat : circuitbreaker event 추가

--- external-api/src/main/java/com/company/external/config/CircuitBreakerConfig.java
+++ external-api/src/main/java/com/company/external/config/CircuitBreakerConfig.java
@@ -20,8 +20,20 @@ public class CircuitBreakerConfig {
             @Override
             public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
                 log.info("RegistryEventConsumer.onEntryAddedEvent");
-                entryAddedEvent.getAddedEntry().getEventPublisher().onEvent(event -> log.info(event.toString()));
-                entryAddedEvent.getAddedEntry().getEventPublisher().onFailureRateExceeded(event -> log.info("{}", event.getEventType()));
+
+                CircuitBreaker.EventPublisher eventPublisher = entryAddedEvent.getAddedEntry().getEventPublisher();
+
+                /**
+                 * 각각의 이벤트들 전부 넣는 것이 아니라 서비스에 따른 적재적소로 적용 <br>
+                 */
+                eventPublisher.onEvent(event -> log.info("onEvent {}", event));
+                eventPublisher.onSuccess(event -> log.info("onSuccess {}", event));
+                eventPublisher.onCallNotPermitted(event -> log.info("onCallNotPermitted {}", event));
+                eventPublisher.onError(event -> log.info("onError {}", event));
+                eventPublisher.onIgnoredError(event -> log.info("onIgnoredError {}", event));
+                eventPublisher.onStateTransition(event -> log.info("onStateTransition {}", event));
+                eventPublisher.onSlowCallRateExceeded(event -> log.info("onSlowCallRateExceeded {}", event));
+                eventPublisher.onFailureRateExceeded(event -> log.info("onFailureRateExceeded {}", event));
 
             }
 
