8fbc323377ee21895d61444cf9e3e8c4166e5eb5
albbloomer <a_l_b_bloomer@naver.com>
2024-02-12 09:21:14 +0900

    docs : resilience4j 를 쓰지않았을 떄, 순수 하드코딩 주석

--- external-api/src/main/java/com/company/external/service/RetryService.java
+++ external-api/src/main/java/com/company/external/service/RetryService.java
@@ -16,6 +16,36 @@ public class RetryService {
         return callAnotherServer(param);
     }
 
+    /**
+     *     Todo : 해당 부분은 만약 resilience4j 를 쓰지 않았을 떄, 순수 하드 코딩 <br>
+     *     private static final int MAX_ATTEMPS = 3;
+     *     private static final int WAIT_DURATION = 1000;
+     *
+     *     public String process(String param) throws InterruptedException {
+     *         String result = null;
+     *
+     *         int retryCount = 0;
+     *
+     *         while (retryCount++ < MAX_ATTEMPS) {
+     *             try {
+     *                 result = callAnotherServer(param);
+     *             } catch (RetryException ex) {
+     *                 if (retryCount == MAX_ATTEMPS) {
+     *                     return fallback(param, ex);
+     *                 }
+     *
+     *                 Thread.sleep(WAIT_DURATION);
+     *             }
+     *
+     *             if (result != null)
+     *                 break;
+     *         }
+     *
+     *         return result;
+     *     }
+     */
+
+
     private String fallback(String param, Exception ex) {
         // retry에 전부 실패해야 fallback이 실행
         log.info("fallback! your request is " + param);
