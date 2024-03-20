5d8173d3aa870f3ce2e2577ceca79dc5dafbfe6a
albbloomer <a_l_b_bloomer@naver.com>
2024-03-20 10:47:13 +0900

    feat : session

--- store-api/src/main/java/com/company/store/practiceandrefactoring/controller/SessionController.java
+++ store-api/src/main/java/com/company/store/practiceandrefactoring/controller/SessionController.java
@@ -1,13 +1,27 @@
 package com.company.store.practiceandrefactoring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SessionController {
 
-    @RequestMapping("/session")
-    public String helloAdmin() {
-        return "hello admin";
+    @GetMapping("/value/{value}")
+    public ResponseEntity<Object> setSessionResult(
+            HttpSession httpSession,
+            @PathVariable("value") String value
+    ) {
+        Object attribute = httpSession.getAttribute("value");
+        httpSession.setAttribute("value", value);
+        return ResponseEntity.ok(attribute);
+    }
+
+    @GetMapping("/session")
+    public ResponseEntity<Object> getSessionResult(HttpSession httpSession) {
+        Object attribute = httpSession.getAttribute("value");
+        return ResponseEntity.ok(attribute);
     }
 }
