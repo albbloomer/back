b9ece91e285570e10c8247dc7d684e6291abd017
albbloomer <a_l_b_bloomer@naver.com>
2024-04-04 10:19:20 +0900

    feat : redis 에 필요한 bean 만 로드 및 출력

--- store-infrastructure/store-redis/src/test/java/com/company/store/infrastructure/redis/service/RedisStringTest.java
+++ store-infrastructure/store-redis/src/test/java/com/company/store/infrastructure/redis/service/RedisStringTest.java
@@ -1,19 +1,40 @@
 package com.company.store.infrastructure.redis.service;
 
+import com.company.store.infrastructure.redis.EmbeddedRedisTestConfig;
+import com.company.store.infrastructure.redis.config.RedisConfig;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ContextConfiguration(classes = {
+        EmbeddedRedisTestConfig.class,
+        RedisConfig.class,
+        RedisString.class
+})
 @ActiveProfiles("test")
 @SpringBootTest
 public class RedisStringTest {
 
     @Autowired
     private RedisString redisString;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void testApplicationContext() {
+        assertNotNull(applicationContext);
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
 
     @Test
     public void testGet() {
