87f00cfddd723580fb3a7c4754b14235470f713d
albbloomer <a_l_b_bloomer@naver.com>
2024-04-04 09:56:38 +0900

    feat : redis 테스트 환경 셋팅 완료

---
+++ store-infrastructure/store-redis/src/test/java/com/company/store/infrastructure/redis/EmbeddedRedisTestConfig.java
@@ -0,0 +1,29 @@
+package com.company.store.infrastructure.redis;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+@Configuration
+public class EmbeddedRedisTestConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
--- store-infrastructure/store-redis/src/test/java/com/company/store/infrastructure/redis/service/RedisStringTest.java
+++ store-infrastructure/store-redis/src/test/java/com/company/store/infrastructure/redis/service/RedisStringTest.java
@@ -1,33 +1,19 @@
 package com.company.store.infrastructure.redis.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import redis.embedded.RedisServer;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class RedisStringTest {
 
-    private static RedisServer redisServer;
-
     @Autowired
     private RedisString redisString;
-
-    @BeforeEach
-    public void setUp() {
-        redisServer = new RedisServer(6379);
-        redisServer.start();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        redisServer.stop();
-    }
 
     @Test
     public void testGet() {
