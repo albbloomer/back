7d40e2ca087e898dceb3fa821d869fa4d171caef
albbloomer <a_l_b_bloomer@naver.com>
2024-05-07 21:18:34 +0900

    docs : ValidateDataException 주석

--- store-common/src/main/java/com/company/store/common/exception/ValidateDataException.java
+++ store-common/src/main/java/com/company/store/common/exception/ValidateDataException.java
@@ -1,5 +1,9 @@
 package com.company.store.common.exception;
 
+/**
+ * RunTimeException 이 아닌 그것을 상속한 IllegalArgumentException 를 함으로써 장점은
+ * '코드가 주는 명확함'
+ */
 public class ValidateDataException extends IllegalArgumentException {
 
     public ValidateDataException(final String exceptionMessage) {
