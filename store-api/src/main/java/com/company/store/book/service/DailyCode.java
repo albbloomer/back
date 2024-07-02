d105d2f5303f99667443fb99a6e03c0dc712c7e0
albbloomer <a_l_b_bloomer@naver.com>
2024-07-03 00:05:19 +0900

    feat : DailyCode 추가

---
+++ store-api/src/main/java/com/company/store/book/service/DailyCode.java
@@ -0,0 +1,14 @@
+package com.company.store.book.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DailyCode {
+    /**
+     * <p>
+     *     하루에 1개라도 좋으니 좋은 코드를 1개씩 보려고 노력하고 여기에 적읍이다.
+     * </p>
+     *
+     * @author min yul
+     */
+}
