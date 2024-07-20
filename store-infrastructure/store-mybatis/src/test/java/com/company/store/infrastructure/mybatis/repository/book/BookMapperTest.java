677ae8c126d6b6f169c643c89ed8a1fe13afccc2
albbloomer <a_l_b_bloomer@naver.com>
2024-07-20 15:20:18 +0900

    refactor : spring mybatis 테스트 환경 수정

--- store-infrastructure/store-mybatis/src/test/java/com/company/store/infrastructure/mybatis/repository/book/BookMapperTest.java
+++ store-infrastructure/store-mybatis/src/test/java/com/company/store/infrastructure/mybatis/repository/book/BookMapperTest.java
@@ -1,20 +1,43 @@
 package com.company.store.infrastructure.mybatis.repository.book;
 
-import com.company.store.infrastructure.mybatis.repository.config.RepositoryTestConfiguration;
+import com.company.store.domain.book.Book;
+
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Import(RepositoryTestConfiguration.class)
+@SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SuppressWarnings({"NonAsciiCharacters"})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BookMapperTest {
+
+    @Autowired BookMapper bookMapper;
+
+    @Nested
+    class 가져올때 {
+
+        @Test
+        void 정상적으로_가져온다() {
+
+            // expected result
+            String exceptedBookName = "abc";
+
+            // when
+            Book book = bookMapper.findById(1L);
+
+            // then
+            assertThat(book.getName()).isEqualTo(exceptedBookName);
+        }
+    }
 
 }
