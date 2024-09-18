ab1994fc0b95ea3ba8da7648534098d3eeb03f00
albbloomer <a_l_b_bloomer@naver.com>
2024-09-18 23:00:08 +0900

    feat : 알고리즘 시작

---
+++ cote/src/main/java/com/company/programers/newchallenge/NewChallenge.java
@@ -0,0 +1,20 @@
+package com.company.programers.newchallenge;
+
+import java.util.HashMap;
+
+public class NewChallenge {
+    /**
+     * 프로그래머스 고득점 킷을 꾸준히 풀어보자.
+     */
+    static class Solution_1 {
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+
+            return answer;
+        }
+    }
+
+    // Todo : 2Q
+
+    // Todo : 3Q
+}
---
+++ cote/src/main/java/com/company/programers/newchallenge/NewPractice.java
@@ -0,0 +1,13 @@
+package com.company.programers.newchallenge;
+
+public class NewPractice {
+
+    /**
+     * 연습용
+     */
+    public static void main(String[] args) {
+
+        int answer = 1;
+        System.out.println("answer : " + answer);
+    }
+}
